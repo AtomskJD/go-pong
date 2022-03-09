f1d5ef1395c450738005da5e7e6f450b0295c8d9
atomsk <atomskjd@gmail.com>
2022-03-09 19:56:05 +0500

    Tasks

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+// TODO: how to render in console
+// TODO: Draw paddles
+// TODO: Player movements
+// TODO:Paddle boundaries
+// TODO:Draw ball
+// todo:update ball movement
+// todo: handle collisions
+// todo: handle GAMEOVERS
 package main
 
 func main() {
